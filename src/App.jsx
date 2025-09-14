b00e99c2419b8db9f49260e4a4dc2de07bd4c506
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-09-14 21:10:05 +0600

    started working with the api first

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <>
+    <div>
+      <h1>Hello Weather app</h1>
+    </div>
+    </>
+  )
 }
 
 export default App;
+
