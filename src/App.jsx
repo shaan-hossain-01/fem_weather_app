6575c46852531a2c794eeae9dd87c3d0e2b6c4a2
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-09-17 12:30:33 +0600

    created design tokens for fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
 import { getWeatherData } from "./api/weatherService";
+
 function App() {
   getWeatherData("New York");
   return (
     <>
-      <div>
-        <h1 className="text-8xl text-blue-900">Hello Weather app</h1>
+      <div className="bg-neutral-900 text-neutral-0 p-4">
+        <h1 className="text-orange-500 font-heading">Weather App</h1>
+        <p className="text-neutral-300">Temperature details</p>
+        <button className="bg-blue-500 text-neutral-0 px-4 py-2 rounded">
+          Get Weather
+        </button>
       </div>
     </>
   );
