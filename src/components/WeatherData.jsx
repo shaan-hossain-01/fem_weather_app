9de1e2e26add4c2db2f072d3a2b717237e3dbeb8
Atiq Afzal Hossain Masud <rockshaan200@gmail.com>
2025-09-18 21:02:15 +0600

    destructuring the data

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -9,8 +9,18 @@ const WeatherData = ({ data }) => {
       <div className="bg-red-600 h-full w-full col-span-2 flex flex-col gap-8">
         {/* Weather Information will go here */}
         <div className="flex-2 flex flex-col gap-8">
-          <div className="flex-2 bg-yellow-300">
-            <h2 className="text-3xl"></h2>
+          <div className="flex-2 bg-yellow-300 flex justify-between items-center p-6">
+            <h2 className="text-3xl text-gray-800">
+              {data.location ? data.location.split(",")[0] : ""}
+            </h2>
+            <div className="flex gap-2">
+              <p className="text-black text-7xl">
+                {data.current ? Math.round(data.current.temperature_2m) : ""}
+              </p>
+              <p className="text-black text-7xl">
+                {data.current_units?.temperature_2m}
+              </p>
+            </div>
           </div>
           <div className="flex-1 bg-yellow-300 flex gap-6">
             <div className="bg-orange-500 flex-1"></div>
